c3f132c6757d43df179fdc7855bb2de1452494db
Meredith Finkelstein <meredith@printallover.me>
2019-01-22 16:54:09 -0500

    WORKING BUT NOT WORKING

---
+++ README.md
@@ -0,0 +1,9 @@
+#Implemenation of [cryptopals][https://cryptopals.com] in [Rust[https://doc.rust-lang.org]
+=======
+
+### Set 1 Problem 1
+
+This implementation is not optimized. I spent most of the time learning Rust. I originally implemented this with vectors of chars, encountered 'use of moved value' error
+switched to String, continued to get the error and then realized that one of my function definitions was not mutable. Towards the end I discovered the chunk method, which I could probably
+use to optimize this code. I would also use some bitwise operations instead of looping and 2^... Also this is not Rust like since I use too many conditional not enough pattern matching, but 
+its a first step.  The interactive mode does not work and the padding is a hot mess but there it is.
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::iter::FromIterator;
 use std::collections::HashMap;
 use std::vec::Vec;
 fn main() {
@@ -11,52 +12,44 @@ fn q_1_1(){
 //Cryptopals Set 1 Challenge 1
 fn hex2base64(){
     let hexmap: HashMap<&str,&str> = [("0","0000"),("1","0001"),("2","0010"),("3","0011"),("4","0100"),("5","0101"),("6","0110"),("7","0111"),("8","1000"),("9","1001"),("A","1010"),("B","1011"),("C","1100"),("D","1101"),("E","1110"),("F","1111")].iter().cloned().collect();
-//49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+    let _test = String::from("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
+
     let mut _input = String::new();
     let mut _output = String::new();
+
+    let mut _mode =String::new();
+    println!("Interactive Y/N?");
     io::stdin().read_line(&mut _input)
         .expect("Failed to read line");
-    let mut tmp_vec: Vec<String> = Vec::with_capacity(6);
+    if _mode != "N"{
+        io::stdin().read_line(&mut _input)
+        .expect("Failed to read line");
+    }else{
+        _input=String::from("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
+    }
+
+    let mut bytes: Vec<String> = Vec::new();
     let mut output: Vec<char> = Vec::new();
     for c in _input.chars(){
         if let Some(&byte) =  hexmap.get::<str>(&c.to_string().to_uppercase()) {
             let mut byte_vec: Vec<String> = byte.chars().map(|x| x.to_string()).collect();
-            println!("{:?}",tmp_vec.len());
-            let rem = (24 - tmp_vec.len()) - 1; //dont need mod
-            if rem < 4 {
-                    tmp_vec.append(&mut byte_vec.get(0..rem).unwrap().iter().cloned().collect());
-            }else{
-                    tmp_vec.append(&mut byte_vec);
-            }
-            if tmp_vec.len() == 24{
-                        let mut ap = bin2dec(&mut tmp_vec).chars().collect();
-                        output.append(&mut ap);
-                        tmp_vec.truncate(0);
-            }
-            if rem < 4 {
-                    tmp_vec.append(&mut byte_vec.get(rem..byte_vec.len()).unwrap().iter().cloned().collect());
-            }
-                //println!("LEN ELSE {:?} {:?}", tmp_vec.len(),tmp_vec);
-        
+            bytes.append(&mut byte_vec);
         }
     }
-    if tmp_vec.len() > 0 {
-        //tmp_vec.resize_with(24,||{p = "0"; p});//may not always be nexessary
-        let finalrem: i32 = tmp_vec.len() as i32 % 24;
-        let mut zero =  vec![String::from("0")];
-        for _i in 0..finalrem {//bogus
-                    tmp_vec.append(&mut zero);
-        }
-        let mut ap = bin2dec(&mut tmp_vec).chars().collect();
+    for b in bytes.chunks(24){
+        let b_len = b.len();
+        //this padding is a hot mess 
+        let mut b_vec = b.to_vec();
+        let mut ap = bin2dec(&b_vec).chars().collect();
         output.append(&mut ap);
     }
-            
-        //let index = hex.iter().position(|&r| r == c).unwrap();
+    let finaloutput = String::from_iter(output);
 
-        
- //   let mut _b_input = _input.as_bytes();
-
-    println!("In Base64 that is {:?}", output);
+    println!("In Base64 that is {:?}", finaloutput);
+    if _mode == "N"{
+        assert!(finaloutput == _test);
+    }        
+    
 }
 fn bin2dec(mut _byte_vec: &Vec<String>) -> (String){
     let mapping: &'static str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="; //should be global figure that out
@@ -64,21 +57,30 @@ fn bin2dec(mut _byte_vec: &Vec<String>) -> (String){
     
     let mut s: i32 = 0;
     let mut i: i32 = 0;    
+    let mut zero =  vec![String::from("0")];
     for bit in _byte_vec.chunks(6){
-        if i == 6 {
-            base64.push(mapping.chars().nth(s as usize).unwrap()); //.unwrap().to_string());
-            println!("Tst Base64 that is {:?}, {:?}", s,base64);
-            i = 0;
-            s = 0;
+        let mut bit_vec = bit.to_vec();
+        //println!("{:?}", bit_vec);
+        i = 0; 
+        s = 0; 
+        let len = 6-bit_vec.len();
+        for _i in 0..len{
+            bit_vec.append(&mut zero.iter().cloned().collect());
         }
-        if bit == "1" {
-            println!("Tst is {:?}, {:?}", s,i);
-            s += i32::pow(2,i as u32);
+        //println!("sizel is {:?}", bit_vec.len());
+       // println!("size is {:?}", bit_vec);
+        bit_vec.reverse();
+        for b in bit_vec{
+
+            if b == "1"{
+                s += i32::pow(2,i as u32);
+            }
+            i+=1;
         }
-        i += 1;
+        base64.push(mapping.chars().nth(s as usize).unwrap()); //.unwrap().to_string());
+        //println!("SUM {:?}, {:?}", s,base64);
     }
-    base64.push(mapping.chars().nth(s as usize).unwrap()); //.unwrap().to_string());
-    println!("Tst Base64 that is {:?} {:?} {:?}", s,_byte_vec,base64);
+    //println!("Tst Base64 that is {:?} {:?} {:?}", s,_byte_vec,base64);
     //for i <6 add ==
     return base64;
 }
