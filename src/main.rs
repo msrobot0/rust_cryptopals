8038ac98fe3e705879e723134dbdfe9f8abf7e37
Meredith Finkelstein <meredith@printallover.me>
2019-01-22 12:59:00 -0500

    BEFORE I TOTALLY SCREW EVERYTHING UP

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,18 @@ fn hex2base64(){
     for c in _input.chars(){
         if let Some(&byte) =  hexmap.get::<str>(&c.to_string().to_uppercase()) {
             let mut byte_vec: Vec<String> = byte.chars().map(|x| x.to_string()).collect();
+            let rem = if tmp_vec.len() %24; 
+            if rem > 0{
+
+                tmp_vec.append(&mut byte_vec.get(0..2).unwrap().iter().cloned().collect());
+                
+            }
+
+            if (tmp_vec.len() == 24){}
+                        let mut ap = bin2dec(&mut tmp_vec).chars().collect();
+                        output.append(&mut ap);
+                        tmp_vec.truncate(0);
+            }
             println!("LEN {:?}", tmp_vec.len());
             if tmp_vec.len() ==4 {
                 //let mut u: Vec<char> = byte_vec.splice(..2,new.iter().cloned()).collect(); //not good rust
@@ -31,25 +43,29 @@ fn hex2base64(){
                 tmp_vec.append(&mut byte_vec.get(2..4).unwrap().iter().cloned().collect()); //how do i do [2:]
             }else{
                 tmp_vec.append(&mut byte_vec);
+                println!("LEN ELSE {:?} {:?}", tmp_vec.len(),tmp_vec);
                 if tmp_vec.len() == 6 {
-                        let mut ap = bin2dec(&mut tmp_vec).chars().collect();
-                        output.append(&mut ap);
-                        tmp_vec.truncate(0);
                 }
             
             }
         }
+    }
+    if tmp_vec.len() > 0 {
+        tmp_vec.append(&mut vec!["0".to_string(),"0".to_string()]);
+        let mut ap = bin2dec(&mut tmp_vec).chars().collect();
+        output.append(&mut ap);
+    }
+            
         //let index = hex.iter().position(|&r| r == c).unwrap();
 
         
-    }
  //   let mut _b_input = _input.as_bytes();
 
     println!("In Base64 that is {:?}", output);
 }
 fn bin2dec(mut _byte_vec: &Vec<String>) -> (String){
-    let mapping: &'static str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/ =";
-    ///println!("Tst Base64 that is {:?} {:?}", _byte_vec.len(),_byte_vec);
+    let mapping: &'static str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    println!("Tst Base64 that is {:?} {:?}", _byte_vec.len(),_byte_vec);
     
     let mut s: i32 = 0;
     let mut i: i32 = 0;
